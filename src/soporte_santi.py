a78f42d28f9caa7c9d559ebeacf18debcd74feb3
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-11-11 09:03:36 +0100

    .

--- src/soporte_santi.py
+++ src/soporte_santi.py
@@ -1,71 +1,131 @@
-def scrap_fuerte(Sopa_produc):
-    """ Extrae datos de productos frescos desde un objeto BeautifulSoup. 
+from bs4 import BeautifulSoup # type: ignore
+import requests # type: ignore
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
 
-    Args: sopa_productos (BeautifulSoup): Objeto BeautifulSoup que contiene la información de los productos. 
+def datos_rains(url, web):
+    df_rain = pd.DataFrame()
+
+    try:
+        url_sopa = url
+
+        res = requests.get(url_sopa)
+        sopa = BeautifulSoup(res.content, "html.parser")
+
+        resultado = sopa.find("div", {"class": "grid gap-y-6 lg:gap-y-10 mb-18 grid-cols-2 lg:grid-cols-4"})
+        result_tr = resultado.findAll("div", {"class": "product-item @container"})
+        
+        if (res.status_code == 200 and resultado and result_tr):
+ 
+            lista = []
+            titulo = ''
+            precio = ''
+            for tr in result_tr:
+                titulo = tr.find("p", {"class": "w-full @xs:w-auto font-bold"})
+                precio = tr.find("span", {"class": "flex gap-1"})
+
+                lista_inter = []
+                lista_inter.append(titulo,precio,web)
+                lista.append(lista_inter)
+
+        df_rain = pd.DataFrame(lista)
+
+    except:
+        print("error en datos_rains(url)")
+ 
+    return df_rain
+
+def datos_minimalismbrand_camisetas(url, web, categorias):
+    df_rain = pd.DataFrame()
+
+    #try:
+    url_sopa = url
+
+    res = requests.get(url_sopa)
+    sopa = BeautifulSoup(res.content, "html.parser")
+
+    #resultado = sopa.find("div", {"class": "product-list product-list--per-row-3 product-list--per-row-mob-2 product-list--per-row-mob-2 product-list--image-shape-shortest"})
+    #print(resultado)
+    result_tr = sopa.findAll("div", {"class": "product-info"})
     
-    Returns: lista_productos (list): 
-                                    Lista de listas, donde cada sublista contiene datos de un producto. 
-    La función extrae el nombre del producto, el precio, el descuento, y el precio unitario de cada producto. """
+    #if (res.status_code == 200 and resultado and result_tr):
+
+    lista = []
+    titulo = ''
+    precio = ''
+    for tr in result_tr:
+        try:
+            titulo = str(tr.find("h3").text).strip()
+        except:
+            titulo = ''
+        try:
+            precio = str(tr.find("div", {"class": "product-price"}).text).strip()
+            precio = precio.split("\n")[0]
+        except:
+            precio = '0'
+
+        if (titulo and precio):
+            lista_inter = []
+            lista_inter.append(titulo)
+            lista_inter.append(precio)
+            lista_inter.append(web)
+            lista_inter.append(categorias)
+            
+            lista.append(lista_inter)
+
+    df_rain = pd.DataFrame(lista)
+
+    #except:
+    #    print("error en datos_minimalismbrand(url)")
+ 
+    return df_rain
+
+def datos_minimalismbrand_cullote(url, web, categorias):
+    df_rain = pd.DataFrame()
+
+    #try:
+    url_sopa = url
+
+    res = requests.get(url_sopa)
+    sopa = BeautifulSoup(res.content, "html.parser")
+
+    #resultado = sopa.find("div", {"class": "product-list product-list--per-row-3 product-list--per-row-mob-2 product-list--per-row-mob-2 product-list--image-shape-shortest"})
+    #print(resultado)
+    result_tr = sopa.findAll("div", {"class": "product-info"})
     
-    lista_productos = []
-    for e in Sopa_produc:
-        elemento = []
+    #if (res.status_code == 200 and resultado and result_tr):
+
+    lista = []
+    titulo = ''
+    precio = ''
+    for tr in result_tr:
+        try:
+            titulo = str(tr.find("h3").text).strip()
+        except:
+            titulo = ''
+        try:
+            precio = str(tr.find("div", {"class": "product-price"}).text).strip()
+            precio = precio.split("\n")[0]
+        except:
+            pass
         
-        nombre_producto = e.find('h3').get_text()
-        elemento.append(nombre_producto)
-        
-        precio = e.findAll('span', class_='price')[1].get_text()
-        elemento.append(precio)
-        
-        votos = e.findAll('span', class_='votes hide-for-small')[0].get_text()
-        elemento.append(votos)
-        
-        descripcion = e.find('meta', {'itemprop': 'description'}).get('content')
-        elemento.append(descripcion)
-        
-        rating_meta = e.find('meta', {'itemprop': 'ratingValue'})
-        valor_rating = rating_meta.get('content') if rating_meta else None
-        elemento.append(valor_rating)
-        
-        lista_productos.append(elemento)
+        if (titulo):
+            lista_inter = []
+            lista_inter.append(titulo)
+            lista_inter.append(precio)
+            lista_inter.append(web)
+            lista_inter.append(categorias)
+            
+            lista.append(lista_inter)
+
     
-    return lista_productos
+    df_rain = pd.DataFrame(lista)
+
+    #except:
+    #    print("error en datos_minimalismbrand(url)")
+ 
+    return df_rain
 
 
-def productos_frescos(sopa_productos):
-    """
-    Extrae datos de productos frescos desde un objeto BeautifulSoup.
-
-    Args:
-        sopa_productos (BeautifulSoup): Objeto BeautifulSoup que contiene la información de los productos.
-
-    Returns:
-        lista_productos (list): Lista de listas, donde cada sublista contiene datos de un producto.
-    
-    La función extrae el nombre del producto, el precio, el descuento, y el precio unitario de cada producto.
-    """
-    lista_productos = []
-    for e in sopa_productos:
-        elemento = []
-        
-        # Extraer el nombre del producto
-        nombre_producto = e.find('h2').get_text()
-        elemento.append(nombre_producto)
-        
-        # Extraer el precio del producto
-        precio = e.findAll('span', class_='value')[0].get_text()
-        elemento.append(precio)
-        
-        # Extraer el descuento del producto
-        descuento = e.findAll('span', class_='marker')[0].get_text()
-        elemento.append(descuento)
-        
-        # Extraer el precio unitario del producto
-        precio_unitario = e.findAll('div', class_='unit-price-row price')[0].get_text()
-        elemento.append(precio_unitario)
-        
-        lista_productos.append(elemento)
-    
-    return lista_productos
 
 
